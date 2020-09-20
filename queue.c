f2ac5018b8032d2a310e26b0a215cfb7d68387e6
Chwchao <gerald.c.chao@gmail.com>
2020-09-20 16:21:00 +0800

    Change q_size return value to q->size

--- queue.c
+++ queue.c
@@ -80,11 +80,8 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
  * Return 0 if q is NULL or empty
  */
 int q_size(queue_t *q)
-{
-    /* TODO: You need to write the code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return 0;
+{ 
+    return q->size;
 }
 
 /*
